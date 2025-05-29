25566d894b1a978a711f99f8d608ffd62705c824
Elias Jacob <contato@eliasjacob.com.br>
2025-05-29 15:41:14 -0300

    Added Stanford Dogs dataset

--- helpers/transformer.py
+++ helpers/transformer.py
@@ -170,7 +170,7 @@ def get_training_arguments(output_dir: str = './tmp/ner', learning_rate: float =
         logging_steps=1,
         eval_steps=1,
         save_steps=1,
-        evaluation_strategy="epoch",
+        eval_strategy="epoch",
         save_strategy='epoch',
         load_best_model_at_end=True,
         metric_for_best_model="eval_f1",
